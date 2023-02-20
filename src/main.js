6b7870e41e1bae357fc7bccfe91df740cdeecda5
joe_2m <josseymunyi@gmail.com>
2023-02-20 11:06:19 +0300

    resolve dependency problem

--- src/main.js
+++ src/main.js
@@ -1,20 +1,20 @@
-import Vue from 'vue';
-import App from './App.vue';
-import vuetify from './plugins/vuetify';
-import router from './router';
-import store from './store';
+import Vue from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import router from "./router";
+import store from "./store";
 
 //Plugins
-import '@/plugins/date-format';
+// import "@/plugins/date-format";
 
 // add axios global instance
-import api from './services/api';
+import api from "./services/api";
 Vue.prototype.$http = api;
 
 // register service worker for PWA support
-import wb from './registerServiceWorker';
+import wb from "./registerServiceWorker";
 
-import i18n from './i18n';
+import i18n from "./i18n";
 Vue.prototype.$workbox = wb;
 
 Vue.config.productionTip = false;
@@ -25,4 +25,4 @@ new Vue({
   store,
   i18n,
   render: (h) => h(App),
-}).$mount('#app');
+}).$mount("#app");
